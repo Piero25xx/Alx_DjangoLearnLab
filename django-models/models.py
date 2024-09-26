29bbd5fd6a6f0b3fd3e5a9a422e38a4f08b4c933
Pierre Mikhael <85271778+Piero25xx@users.noreply.github.com>
2024-09-26 13:25:56 +0300

    Update models.py

--- django-models/models.py
+++ django-models/models.py
@@ -1,9 +1,32 @@
 from django.db import models
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+# Author model with a simple CharField
+class Author(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return f'{self.title} by {self.author} ({self.publication_year})'
+        return self.name
+
+# Book model with a ForeignKey to Author
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+# Library model with a ManyToMany relationship to Book
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+# Librarian model with a OneToOne relationship to Library
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
