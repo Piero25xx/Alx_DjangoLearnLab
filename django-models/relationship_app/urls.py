548f014410e29152bf7461e409167347372a1733
Pierre Mikhael <85271778+Piero25xx@users.noreply.github.com>
2024-09-26 14:01:50 +0300

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from views import list_books
+from .views import list_books
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),  # FBV for listing books
