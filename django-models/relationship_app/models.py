11843ee732b7f7a6382bea5002a0850a0e2109b6
Pierre Mikhael <85271778+Piero25xx@users.noreply.github.com>
2024-09-26 20:28:30 +0300

    Update models.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Author model with a simple CharField
 class Author(models.Model):
@@ -51,3 +55,25 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
+
+ Choices for user roles
+ROLE_CHOICES = [
+    ('Admin', 'Admin'),
+    ('Librarian', 'Librarian'),
+    ('Member', 'Member'),
+]
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+# Signal to create or update the UserProfile whenever a User is created or updated
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
