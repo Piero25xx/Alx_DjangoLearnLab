ac812a5df4cb54bed68816cdec05339268d7b2b4
Pierre Mikhael <85271778+Piero25xx@users.noreply.github.com>
2024-09-29 21:14:21 +0300

    Update models.py

--- advanced_features_and_security/models.py
+++ advanced_features_and_security/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Author model with a simple CharField
 class Author(models.Model):
@@ -51,3 +54,45 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
+# Custom User Manager
+class CustomUserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and return a regular user with an email and password."""
+        if not email:
+            raise ValueError(_('The Email field must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and return a superuser with an email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+
+        return self.create_user(email, password, **extra_fields)
+
+# Custom User Model
+class CustomUser(AbstractUser):
+    username = None  # remove username field
+    email = models.EmailField(_('email address'), unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_of_birth']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
