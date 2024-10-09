91ee3a8861666287121f453b761971cbe764aaf8
piero25xx <pierremhanna3@gmail.com>
2024-10-09 18:12:36 +0300

    edit view.py

--- api_project/views.py
+++ api_project/views.py
@@ -1,1 +1,7 @@
+rom rest_framework.generics import ListAPIView  # Import ListAPIView from Django REST Framework
+from .models import Book
+from .serializers import BookSerializer
 
+class BookList(ListAPIView):
+    queryset = Book.objects.all()  # Retrieve all Book instances
+    serializer_class = BookSerializer  # Use BookSerializer to serialize the data
