430af577b71c68e313a25c9792e320907fc2783c
piero25xx <pierremhanna3@gmail.com>
2024-10-15 13:39:48 +0300

    admin auth

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include 
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Admin URL
     path('api/', include('api.urls')),  # Include the URLs from your api app
+    path('api/token/', obtain_auth_token, name='api_token_auth'),
 ]
--- api_project/views.py
+++ api_project/views.py
@@ -1,7 +1,25 @@
-rom rest_framework.generics import ListAPIView  # Import ListAPIView from Django REST Framework
+from rest_framework.generics import ListAPIView  # Import ListAPIView from Django REST Framework
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.viewsets import ModelViewSet
+from .models import ExampleModel
+from .serializers import ExampleModelSerialize
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()  # Retrieve all Book instances
     serializer_class = BookSerializer  # Use BookSerializer to serialize the data
+
+class ExampleModelViewSet(ModelViewSet):
+
+    """
+    Viewset for ExampleModel.
+
+    Permissions:
+    - Authenticated users can view and edit their own records.
+    - Admin users have full access to all records.
+    """
+
+    queryset = ExampleModel.objects.all()
+    serializer_class = ExampleModelSerializer
+    permission_classes = [IsAuthenticated]  # Restrict access to authenticated userscla
