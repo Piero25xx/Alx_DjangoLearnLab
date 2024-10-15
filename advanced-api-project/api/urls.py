8461d71acbe988aff8993c218fddb444ebf0c064
Pierre Mikhael <85271778+Piero25xx@users.noreply.github.com>
2024-10-15 19:14:33 +0300

    modified views and urls

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import AuthorList, AuthorDetail, BookList, BookDetail
 
 urlpatterns = [
-    path('authors/', AuthorList.as_view(), name='author-list'),
-    path('authors/<int:pk>/', AuthorDetail.as_view(), name='author-detail'),
-    path('books/', BookList.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetail.as_view(), name='book-detail'),
+    path('authors/', AuthorListView.as_view(), name='author-list'),
+    path('authors/<int:pk>/', AuthorDetailView.as_view(), name='author-detail'),
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('api/', include('api.urls')),
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,22 +4,22 @@ from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 
 
-class AuthorList(generics.ListCreateAPIView):
-    """API view to list and create authors."""
+class AuthorListView(generics.ListCreateAPIView):
+    """View to list and create authors."""
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
 
-class AuthorDetail(generics.RetrieveUpdateDestroyAPIView):
-    """API view to retrieve, update, or delete an author."""
+class AuthorDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """View to retrieve, update, or delete an author."""
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
 
-class BookList(generics.ListCreateAPIView):
-    """API view to list and create books."""
+class BookListView(generics.ListCreateAPIView):
+    """View to list and create books."""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookDetail(generics.RetrieveUpdateDestroyAPIView):
-    """API view to retrieve, update, or delete a book."""
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """View to retrieve, update, or delete a book."""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
