efe0fd2cfe94406b2c35aa9470b18083f36ee9bd
piero25xx <pierremhanna3@gmail.com>
2024-10-16 21:21:41 +0300

    edited url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,19 +9,20 @@ from .views import CommentCreateView, CommentUpdateView, CommentDeleteView
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
     path('', index, name='index'),  # Home page
-     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     
     path('', PostListView.as_view(), name='post-list'),  # List all posts
-    path('post/new/', PostCreateView.as_view(), name='post-create'),  # Create a new post
+    path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # View post details
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-edit'),  # Edit post
+    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # Delete post
     path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
     path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
-
 ]
