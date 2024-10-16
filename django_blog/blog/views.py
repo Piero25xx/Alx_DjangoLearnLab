257fc81f3f3a8aefd8ef120eb00f78319c5f757d
piero25xx <pierremhanna3@gmail.com>
2024-10-16 20:54:48 +0300

    edited

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -48,18 +48,20 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     form_class = PostForm
     template_name = 'blog/post_form.html'
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['view'] = {'action': 'Create'}
+        return context
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'blog/post_form.html'
 
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['view'] = {'action': 'Edit'}
+        return context
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
